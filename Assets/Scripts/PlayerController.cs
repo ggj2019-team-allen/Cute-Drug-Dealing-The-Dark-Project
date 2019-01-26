57f0a7eed84feec4543c10cfb4da765a81f13fef
Loon99 <onnloon0630@gmail.com>
2019-01-26 17:10:49 +0800

    Merge branch 'Movement-Branch'

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float Xspeed = 5f;
+   public float Yspeed = 5f;
+    private float movement = 0f;
+    private Rigidbody2D rigidBody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float movement = Input.GetAxis("Horizontal");
+        float movementY = Input.GetAxis("Vertical");
+        //float movementVertical = Input.GetAxis("Vertical");
+        rigidBody.velocity = new Vector2(movement * Xspeed, movementY * Yspeed);
+ 
+    }
+}
