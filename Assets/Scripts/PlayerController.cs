8375850159498b603ec3f7ac0aebd161daa405f6
Loon99 <onnloon0630@gmail.com>
2019-01-26 17:29:41 +0800

    Movement with Collision

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,12 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class PlayerController : MonoBehaviour
 {
 
     public float Xspeed = 5f;
-   public float Yspeed = 5f;
+    public float Yspeed = 5f;
     private float movement = 0f;
+    public float xMin, xMax, yMin, yMax;
     private Rigidbody2D rigidBody;
     // Start is called before the first frame update
     void Start()
@@ -20,8 +22,12 @@ public class PlayerController : MonoBehaviour
     {
         float movement = Input.GetAxis("Horizontal");
         float movementY = Input.GetAxis("Vertical");
-        //float movementVertical = Input.GetAxis("Vertical");
         rigidBody.velocity = new Vector2(movement * Xspeed, movementY * Yspeed);
+
+        rigidBody.position = new Vector2
+        (
+            Mathf.Clamp(rigidBody.position.x, xMin, xMax), Mathf.Clamp(rigidBody.position.y, yMin, yMax)
+         );
  
     }
 }
