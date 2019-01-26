f2ced219bbe643fe53a7b11d251f682d31ab7ea7
FattyMieo <fong2009@live.com.my>
2019-01-26 18:32:07 +0800

    Finish Main Menu
    Changed Default resolution for the game

---
+++ Assets/Scripts/MainMenu/StartButtonScript.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;// Required when using Event data.
+
+public class StartButtonScript : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+    public RainbowText rainbowText;
+    public bool heldDown;
+    public float holdTimer;
+    public float maxHoldTime;
+
+    void Update()
+    {
+        if(heldDown)
+        {
+            if(holdTimer >= maxHoldTime)
+            {
+                GameManager.instance.gameLoop.StartGame();
+            }
+            else
+            {
+                holdTimer += Time.deltaTime;
+            }
+        }
+        else
+        {
+            if(holdTimer > 0.0f)
+            {
+                holdTimer -= Time.deltaTime;
+            }
+        }
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        heldDown = true;
+        rainbowText.SetEffectActive(heldDown);
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        heldDown = false;
+        rainbowText.SetEffectActive(heldDown);
+    }
+}
---
+++ Assets/Scripts/Tools/PixelTextFix.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class PixelTextFix : MonoBehaviour
+{
+    public Font font;
+    
+    void Update()
+    {
+        if(font)
+            font.material.mainTexture.filterMode = FilterMode.Point;
+    }
+}
