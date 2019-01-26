cb3b57d02441e1cd1e527131ab81e68d81e3cde8
Horashio <gohwengzen@rocketmail.com>
2019-01-26 20:13:09 +0800

    Finished Player Dousing

--- Assets/Scripts/Player_Dousing.cs
+++ Assets/Scripts/Player_Dousing.cs
@@ -7,13 +7,19 @@ public class Player_Dousing : MonoBehaviour
     private Animator anim;
     private Rigidbody2D rb2d;
     private BoxCollider2D boxCollide;
+    private PlayerController pcl;
+    
+    public bool dousing = false;
 
     // Start is called before the first frame update
     void Start()
     {
+        
         anim = GetComponent<Animator>();
         rb2d = GetComponent<Rigidbody2D>();
         boxCollide = GetComponent<BoxCollider2D>();
+        pcl = GetComponent<PlayerController>();
+
     }
 
     // Update is called once per frame
@@ -24,9 +30,21 @@ public class Player_Dousing : MonoBehaviour
 
     void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.gameObject.tag == "NPC")
+        if (other.GetComponent<NPC_Doused>().isDoused == false)
         {
-            anim.SetTrigger("Player_Dousing");
+            if (other.gameObject.tag == "NPC")
+            {
+                anim.SetTrigger("Player_Dousing");
+                rb2d.velocity = new Vector2(0, 0);
+                dousing = true;
+                StartCoroutine(Undouse());
+            }
         }
+    }
+
+    public IEnumerator Undouse()
+    {
+        yield return new WaitForSeconds(0.5f);
+        dousing = false;
     }
 }
