a03b5db4c42956d8a9705b3d903677d9fb99575a
Tung Zie Yeong <ohqoo123@gmail.com>
2019-01-26 20:22:48 +0800

    NPC Walking and Spawner

---
+++ Assets/Scripts/SpawnerLeft.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnerLeft : MonoBehaviour
+
+{
+    public GameObject npc;
+    float randX;
+    Vector2 whereToSpawn;
+    public float spawnRate = 10f;
+    float nextSpawn = 10f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()       
+    {
+        if (Time.time > nextSpawn)
+        {
+            nextSpawn = Time.time +  spawnRate;
+            randX = Random.Range(10.3f, 10.2f);
+            whereToSpawn = new Vector2 (randX, transform.position.y);
+            Instantiate (npc, whereToSpawn, Quaternion.identity);
+        }
+    }
+}
---
+++ Assets/Scripts/SpawnerScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnerScript : MonoBehaviour
+   
+{
+    public GameObject npc;
+    float randX;
+    Vector2 whereToSpawn;
+    public float spawnRate = 5f;
+    float nextSpawn = 10f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()       
+    {
+        if (Time.time > nextSpawn)
+        {
+            nextSpawn = Time.time +  spawnRate;
+            randX = Random.Range(-12.95f, -12.80f);
+            whereToSpawn = new Vector2 (randX, transform.position.y);
+            Instantiate (npc, whereToSpawn, Quaternion.identity);
+        }
+    }
+}
