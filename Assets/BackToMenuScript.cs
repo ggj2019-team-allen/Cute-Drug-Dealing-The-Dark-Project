cee5073bd076ca77236829818dcf92c2dd683077
FattyMieo <fong2009@live.com.my>
2019-01-27 16:26:22 +0800

    Final Changes

---
+++ Assets/BackToMenuScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackToMenuScript : MonoBehaviour
+{
+    public float timer;
+    public float maxTime = 5.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if(timer >= maxTime)
+        {
+            GameManager.instance.gameLoop.QuitGame();
+        }
+    }
+}
